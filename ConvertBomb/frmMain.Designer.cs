aff8443c40d9ac756fad1435ae27f9ec8107d444
pnkl1999 <khanhlam22111999@gmail.com>
2023-11-07 23:44:52 +0700

    first commit

---
+++ ConvertBomb/frmMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ConvertBomb
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtResource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.listError = new System.Windows.Forms.ListView();
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.btnConvert.Location = new System.Drawing.Point(12, 113);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(236, 64);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtResource
+            // 
+            this.txtResource.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.txtResource.Location = new System.Drawing.Point(12, 51);
+            this.txtResource.Name = "txtResource";
+            this.txtResource.Size = new System.Drawing.Size(236, 24);
+            this.txtResource.TabIndex = 1;
+            this.txtResource.Text = "http://res2.gn.zing.vn/";
+            this.txtResource.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Link Resource";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(24, 97);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(65, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Trạng Thái: ";
+            // 
+            // listError
+            // 
+            this.listError.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listError.FullRowSelect = true;
+            this.listError.HideSelection = false;
+            this.listError.Location = new System.Drawing.Point(267, 0);
+            this.listError.Name = "listError";
+            this.listError.Size = new System.Drawing.Size(231, 205);
+            this.listError.TabIndex = 5;
+            this.listError.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Location = new System.Drawing.Point(144, 191);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(120, 13);
+            this.lblAbout.TabIndex = 6;
+            this.lblAbout.Text = "Phạm Ngọc Khánh Lâm";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 205);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.listError);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResource);
+            this.Controls.Add(this.btnConvert);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(514, 244);
+            this.MinimumSize = new System.Drawing.Size(514, 244);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tool Convert Bomb Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtResource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListView listError;
+        private System.Windows.Forms.Label lblAbout;
+    }
+}
---
+++ ConvertBomb/frmMain.cs
@@ -0,0 +1,155 @@
+﻿using Game.Logic.Phy.Maps;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ConvertBomb
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            SetupListView();
+        }
+        private string connectionString = ConfigurationManager.ConnectionStrings["ConvertBomb.Properties.Settings.DDT_Game34ConnectionString"].ConnectionString;
+        private List<string> craterDownloadInfoList = new List<string>();
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            craterDownloadInfoList.Clear();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string selectCommandText = "SELECT * FROM [dbo].[Ball]";
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter(selectCommandText, connection))
+                    {
+                        DataSet dataSet = new DataSet();
+                        dataAdapter.Fill(dataSet, "Groups");
+                        DataTable dataTable = dataSet.Tables["Groups"];
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                int result = 0;
+                                int.TryParse(row["Crater"].ToString(), out result);
+                                if (result > 0)
+                                {
+                                    craterDownloadInfoList.Add(row["Crater"].ToString() + "|" + row["ID"].ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Directory.CreateDirectory("Crater");
+                Directory.CreateDirectory("Bomb");
+                WebClient webClient = new WebClient();
+
+                foreach (string item in craterDownloadInfoList)
+                {
+                    string[] parts = item.Split('|');
+                    string craterDirectory = Path.Combine("Crater", parts[0]);
+
+                    if (!Directory.Exists(craterDirectory))
+                    {
+                        Directory.CreateDirectory(craterDirectory);
+                    }
+
+                    try
+                    {
+                        string url = Path.Combine(txtResource.Text, "image", "bomb", "crater", parts[0], "crater.png");
+                        string destinationPath = Path.Combine(craterDirectory, "crater.png");
+
+                        if (File.Exists(destinationPath))
+                        {
+                            File.Delete(destinationPath);
+                        }
+
+                        await webClient.DownloadFileTaskAsync(url, destinationPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        lblStatus.Text = "Convert " + parts[1] + ".bomb (fail!)";
+                        log.Error("Path Error: " + ex.Message);
+                    }
+
+                    string bombFilePath = Path.Combine("Bomb", parts[1] + ".bomb");
+                    CreateBombFile(Path.Combine(craterDirectory, "crater.png"), bombFilePath, parts[1]);
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblStatus.Text = "Convert " + parts[1] + ".bomb (success!)";
+                    });
+                }
+
+                MessageBox.Show("Convert finish!");
+            }
+            catch (Exception ex)
+            {
+                log.Error("SQL Connect Error : " + ex.Message);
+            }
+            finally
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    btnConvert.Enabled = true;
+                });
+            }
+        }
+        private void SetupListView()
+        {
+            listError.View = View.Details;
+            listError.Columns.Add("Date Time");
+            listError.Columns.Add("File Name");
+
+            listError.Columns[0].Width = 150;
+            listError.Columns[1].Width = 150;
+        }
+
+
+        private void CreateBombFile(string imagePath, string bombFilePath, string filename)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(imagePath);
+                Tile tile = new Tile(bitmap, true);
+
+                using (FileStream fileStream = File.Create(bombFilePath))
+                using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                {
+                    binaryWriter.Write(tile.Width);
+                    binaryWriter.Write(tile.Height);
+                    binaryWriter.Flush();
+                    fileStream.Write(tile.Data, 0, tile.Data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                ListViewItem item = new ListViewItem(DateTime.Now.ToString());
+                item.SubItems.Add($"Convert {filename}.bomb (fail!)");
+                listError.Items.Add(item);
+                lblStatus.Text = "Convert " + filename + ".bomb (fail!)";
+            }
+        }
+    }
+}
